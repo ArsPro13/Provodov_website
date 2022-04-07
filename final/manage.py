3649efa25bd3cfb3d58863ec0027530867f50133
ArsPro13 <Vintbolt13@gmail.com>
2022-04-07 19:34:32 +0300

    Update manage.py

--- final/manage.py
+++ final/manage.py
@@ -4,16 +4,16 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return render_template("index.html")
+    return render_template("final/templates/index.html")
 
 @app.route('/about_us')
 def about_us():
-    return render_template("about_us.html")
+    return render_template("final/templates/about_us.html")
 
 
 @app.route('/catalog')
-def about_us():
-    return render_template("catalog.html")
+def catalog():
+    return render_template("final/templates/catalog.html")
 
 
 
